ff8a7ade3fea6caf18dbab2bbc605ea5d0105b66
Kevin Petersen <kpe@inLogic.dk>
2021-12-01 16:33:07 +0100

    .

--- TabbedPageLoadingIssueIOS/TabbedPageLoadingIssueIOS/App.xaml.cs
+++ TabbedPageLoadingIssueIOS/TabbedPageLoadingIssueIOS/App.xaml.cs
@@ -14,7 +14,7 @@ namespace TabbedPageLoadingIssueIOS
             InitializeComponent();
 
             DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            MainPage = new TabbedTestPage();
         }
 
         protected override void OnStart()
---
+++ TabbedPageLoadingIssueIOS/TabbedPageLoadingIssueIOS/Views/Page2Test.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedPageLoadingIssueIOS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2Test : ContentPage
+    {
+        public Page2Test()
+        {
+            InitializeComponent();
+        }
+    }
+}
